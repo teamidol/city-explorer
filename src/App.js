15307f2131f5d262b613a8ec2e11a9bc70c5c2fd
teamidol <laurencemy@gmail.com>
2023-03-21 23:08:53 -0700

    not done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-
+import Image from 'react-bootstrap/Image';
+import './App.css';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 class App extends React.Component {
   constructor(props) {
@@ -20,6 +22,19 @@ class App extends React.Component {
     })
   }
 
+  handSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/weather?city_name=${this.state.city}`
+
+      let cityData = await axios.get(url);
+
+      console.log(cityData.data);
+    } catch (error) {
+      console.log(error.message);
+    } 
+  }
   // ** async/await - handle our asynchronous code
   // ** try/catch - handle our errors - TRY resolve our successful promises & CATCH handle rejected promise
 
@@ -37,6 +52,7 @@ class App extends React.Component {
       // TODO: Set State with the data that comes back from axios & set error boolean to false
       this.setState({
         cityData: cityDataFromAxios.data[0],
+        mapUrl: `https://maps.locationiq.comv3staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${cityDataFromAxios.data[0].lat},${cityDataFromAxios.data[0].lon}&zoom=13`,
         error: false
       });
 
@@ -66,11 +82,17 @@ class App extends React.Component {
           <button type='submit'>Explore!</button>
         </form>
 
- 
+
         {
           this.state.error
             ? <p>{this.state.errorMessage}</p>
-            : <p>{this.state.cityData.display_name}</p>
+            : Object.keys(this.state.cityData).length > 0 &&
+            <ul>
+              <p id="title">{this.state.cityData.display_name}</p>
+              <Image class="img-fluid" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=13`} alt='Map of location' />
+              <p>{this.state.cityData.lon}</p>
+              <p>{this.state.cityData.lat}</p>
+            </ul>
         }
 
       </>
